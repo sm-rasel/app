d8be761f47b1f7e5d09806b3c045643a4866cec0
Arifuzzaman Rasel <arifuzzamanrasel07@gmail.com>
2024-04-17 17:22:01 +0600

    fixed

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -2,18 +2,18 @@ import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/vue-fontawesome'
-// import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 import store from './store'
 import vueSweetalert2 from 'vue-sweetalert2'
 import {toast} from "vue3-toastify";
 import 'vue3-toastify'
 import 'bootstrap'
 
-import {defineRules} from 'vee-validate'
-import AllRules from '@vee-validate/rules'
+import {defineRule} from 'vee-validate'
+import * as AllRules from '@vee-validate/rules'
 
 Object.keys(AllRules).forEach(rule => {
-    defineRules(rule, AllRules[rule]);
+    defineRule(rule, AllRules[rule]);
 });
 
 import { createApp } from 'vue'
@@ -24,6 +24,6 @@ const app = createApp(App)
 
 app.use(router, store, vueSweetalert2, toast)
 app.config.globalProperties.$store=store
-// app.component("fa", FontAwesomeIcon)
+app.component("fa", FontAwesomeIcon)
 
 app.mount('#app')
